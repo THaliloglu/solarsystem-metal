a41a7c8bfb47589be2402c4cf7113c2c6c2cb1a9
TOLGA HALILOGLU <tolga.haliloglu@turkcell.com.tr>
2023-02-13 20:54:55 +0100

    creates multiplatform target and binds base source code to its target membership.

---
+++ SolarSystem/SolarSystem-multiplatform/SolarSystem_multiplatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolarSystem_multiplatformApp.swift
+//  SolarSystem-multiplatform
+//
+//  Created by TOLGA HALILOGLU on 13.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct SolarSystem_multiplatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
